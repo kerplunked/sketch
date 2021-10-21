de64e3cb914624286a02ce2fca469ed905c4b727
James <jamesncoupland@gmail.com>
2021-10-21 17:04:13 +0100

    only colours furst box

--- script.js
+++ script.js
@@ -5,21 +5,26 @@ const sketchGrid = document.querySelector("#grid");
 
 
 
-//loop append 16 times then go to a new line and 16 again until 16 lines
+//loop to create grid,, may needa new better way
 
 for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
     div.style.height = "32px";
     div.style.width = "32px";
-    div.style.border = "1px solid #000000";
     div.classList.add("box")
+    div.style.border = "1px solid #000000";
+    
     sketchGrid.append(div);
     
 }
 
+
+// draw on one box 
 const draw = document.querySelector(".box");
 
 draw.addEventListener("mouseover", () => {
+    
+    draw.classList.add("box-colour");
     draw.style.backgroundColor = "black";
 
 });
